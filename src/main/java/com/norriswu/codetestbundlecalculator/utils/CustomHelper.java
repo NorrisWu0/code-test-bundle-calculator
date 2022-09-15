eebafa580f8f074166b324cc48422cb174562b78
Norris Wu <42906455+NorrisWu0@users.noreply.github.com>
2022-09-15 22:49:06 +1000

    Remove unused `toSingleton` method from helper

--- src/main/java/com/norriswu/codetestbundlecalculator/utils/CustomHelper.java
+++ src/main/java/com/norriswu/codetestbundlecalculator/utils/CustomHelper.java
@@ -1,38 +1,11 @@
 package com.norriswu.codetestbundlecalculator.utils;
 
-import com.norriswu.codetestbundlecalculator.entity.Bundle;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
 
 public class CustomHelper {
-
-    /**
-     * Apology to whoever provided this on stack overflow, I forgot to include your link here. 🙇
-     */
-    public static <T> Collector<T, ?, T> toSingleton() {
-        return Collectors.collectingAndThen(
-                Collectors.toList(),
-                list -> {
-                    if (list.size() != 1) {
-                        throw new IllegalStateException();
-                    }
-                    return list.get(0);
-                }
-        );
-    }
-
-    /**
-     * Shamelessly stolen from <a href="https://stackoverflow.com/questions/15217438/counting-occurrences-of-a-key-in-a-map-in-java">counting-occurrences-of-a-key-in-a-map-in-java</a>
-     */
-    public static <K> void count(K key, Map<K, Integer> map) {
-        map.merge(key, 1, (currentCount, notUsed) -> ++currentCount);
-    }
-
-    public static List<Integer> findBestCombinationOfNumberGivenList(int required, int currentRemaining, List<Integer> list, int startIndex, int currentIndex, List<Integer> currentCombination) {
+    public List<Integer> findBestCombinationOfNumberGivenList(int required, int currentRemaining, List<Integer> list, int startIndex, int currentIndex, List<Integer> currentCombination) {
         List<Integer> combination = new ArrayList<>(currentCombination);
         int nextBundleIndex = currentIndex;
 
@@ -64,5 +37,12 @@ public class CustomHelper {
                 nextBundleIndex,
                 combination
         );
+    }
+
+    /**
+     * Shamelessly stolen from <a href="https://stackoverflow.com/questions/15217438/counting-occurrences-of-a-key-in-a-map-in-java">counting-occurrences-of-a-key-in-a-map-in-java</a>
+     */
+    public <K> void count(K key, Map<K, Integer> map) {
+        map.merge(key, 1, (currentCount, notUsed) -> ++currentCount);
     }
 }
