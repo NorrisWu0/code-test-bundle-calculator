8301d7315460953e4ad3e35556edb93787fec480
Norris Wu <42906455+NorrisWu0@users.noreply.github.com>
2022-09-15 23:59:23 +1000

    Refactor `BundleCalculator` to process new `Order`

--- src/main/java/com/norriswu/codetestbundlecalculator/services/BundleCalculator.java
+++ src/main/java/com/norriswu/codetestbundlecalculator/services/BundleCalculator.java
@@ -2,6 +2,7 @@ package com.norriswu.codetestbundlecalculator.services;
 
 import com.norriswu.codetestbundlecalculator.entity.Bundle;
 import com.norriswu.codetestbundlecalculator.entity.Order;
+import com.norriswu.codetestbundlecalculator.entity.OrderItem;
 import com.norriswu.codetestbundlecalculator.entity.Product;
 import com.norriswu.codetestbundlecalculator.utils.CustomHelper;
 
@@ -9,13 +10,20 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class BundleCalculator {
-    public void calculate(Order order, Product product) {
-        order.setMatchedBundles(findBestBundleCombination(order, product.getBundles()));
+    public void calculate(Order order, List<Product> products) {
+        order.getItems().forEach(item -> {
+                    products
+                            .stream()
+                            .filter(product -> product.getFormatCode().equals(item.getFormatCode()))
+                            .findFirst()
+                            .ifPresent(matchedProduct -> item.setMatchedBundles(findBestBundleCombination(item, matchedProduct.getBundles())));
+                }
+        );
     }
 
-    private List<Bundle> findBestBundleCombination(Order order, List<Bundle> bundles) {
+    private List<Bundle> findBestBundleCombination(OrderItem orderItem, List<Bundle> bundles) {
         List<Integer> sizeOfBundles = bundles.stream().map(Bundle::getSize).collect(Collectors.toList());
-        List<Integer> bundleIndexCombination = new CustomHelper().findBestCombinationOfNumberGivenList(order.getQuantity(), order.getQuantity(), sizeOfBundles, 0, 0, List.of());
+        List<Integer> bundleIndexCombination = new CustomHelper().findBestCombinationOfNumberGivenList(orderItem.getQuantity(), orderItem.getQuantity(), sizeOfBundles, 0, 0, List.of());
 
         return bundleIndexCombination.stream().map(bundles::get).collect(Collectors.toList());
     }
