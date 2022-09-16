668376d7593369ce2888076f8e36c781c462ea51
Norris Wu <42906455+NorrisWu0@users.noreply.github.com>
2022-09-16 12:22:10 +1000

    Update `Reception` to summarise a given order

--- src/main/java/com/norriswu/codetestbundlecalculator/services/Reception.java
+++ src/main/java/com/norriswu/codetestbundlecalculator/services/Reception.java
@@ -4,9 +4,11 @@ import com.norriswu.codetestbundlecalculator.entity.Order;
 import com.norriswu.codetestbundlecalculator.entity.OrderItem;
 import com.norriswu.codetestbundlecalculator.utils.Logger;
 
+import java.text.NumberFormat;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Reception {
@@ -18,7 +20,27 @@ public class Reception {
                 .map(String::trim)
                 .map(item -> new OrderItem(item.split(" ")))
                 .collect(Collectors.toList());
+        Order order = new Order(orderItems);
 
-        return new Order(orderItems);
+        Logger.info("Okay boss, this is what you have ordered. [" + order + "]");
+
+        return order;
+    }
+
+    public void summariseOrder(Order order) {
+        Logger.info("Okay boss, here's the best deal we can find for you.");
+        NumberFormat currency = NumberFormat.getCurrencyInstance();
+        Logger.info(order + " will cost you " + currency.format(order.getTotal()) + ".");
+        Logger.info("In which...");
+
+        List<OrderItem> orderItems = order.getItems();
+
+        IntStream.range(0, orderItems.size()).forEach(index -> {
+                    OrderItem item = orderItems.get(index);
+                    Logger.info("Item " + index + ": " + item + " will cost " + currency.format(item.getSubTotal()) + " with " + item.listBundle() + ".");
+                }
+        );
+
+
     }
 }
